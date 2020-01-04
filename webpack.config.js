d91e9e36b2d1e9471f28623ca925ac02afc5be32
Kikuo Emoto <kikuomax@gmail.com>
2020-01-04 14:44:14 +0900

    feat: webpack-dev-server
    - `webpack-dev-server` is introduced so that the project is rebuilt
      whenever source code is modified.
    
    issue #3

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ const defaultMode = 'development'
 module.exports = {
   mode: defaultMode,
   entry: './src/index.js',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist')
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
