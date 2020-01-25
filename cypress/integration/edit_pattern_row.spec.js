3c466ab157274e360f05afab225efc06d722a026
Kikuo Emoto <kikuomax@gmail.com>
2020-01-26 01:10:45 +0900

    test(e2e): introduction of Cypress
    - Cypress is introduced for E2E testing.
        - `edit_pattern_row.spec.js` is my first E2E test scenario.
          It simulates that a designer appends a column to the row by dragging
          the row expansion handle of the pattern row editor rightward.
        - Other files and directories in the `cypress` directory are created
          by Cypress.
    
    issue #20

---
+++ cypress/integration/edit_pattern_row.spec.js
@@ -0,0 +1,40 @@
+describe('With a pattern row editor', function () {
+  // pointerId: for PointerEvent simulation.
+  // https://github.com/cypress-io/cypress/issues/5660
+  const pointerId = 1
+  const cellWidth = 50
+
+  beforeEach(function () {
+    cy.visit('/')
+  })
+
+  // Returns a function that triggers a 'pointermove' event
+  // with a new `clientX` relatively shifted by `dX` from
+  // that of a target element.
+  //
+  // Supply a returned function to `cy.then`.
+  function triggerPointermoveBy(dX) {
+    return function (element) {
+      expect(element).to.have.length(1)
+      const { left, width } = element[0].getBoundingClientRect()
+      const clientX = left + Math.floor(width * 0.5) + dX
+      return cy.wrap(element)
+        .trigger('pointermove', { clientX })
+    }
+  }
+
+  it('A designer appends a column to a row by dragging the row expansion handle rightward', function () {
+    cy.get('.row-expansion-handle')
+      .as('handle')
+      .trigger('pointerdown', { pointerId })
+    cy.get('.row-expansion-handle-shape')
+      .as('handle-shape')
+      .should('have.class', 'is-dragged')
+    cy.get('@handle')
+      .then(triggerPointermoveBy(cellWidth))
+    cy.get('@handle')
+      .trigger('pointerup', { pointerId })
+    cy.get('@handle-shape')
+      .should('not.have.class', 'is-dragged')
+  })
+})
