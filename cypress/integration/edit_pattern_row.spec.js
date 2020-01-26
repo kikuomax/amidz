f5ee1aab92818eb0744a8b55f216291dcdd758c9
Kikuo Emoto <kikuomax@gmail.com>
2020-01-27 00:59:19 +0900

    test(e2e): Percy snapshot integration
    - Percy is imported to the Cypress configuration.
    - `cy.percySnapshot` lines are inserted into the scenario
      `edit_pattern_row_spec`.
    
    issue #21

--- cypress/integration/edit_pattern_row.spec.js
+++ cypress/integration/edit_pattern_row.spec.js
@@ -8,6 +8,12 @@ describe('With a pattern row editor', function () {
     cy.visit('/')
   })
 
+  // Returns the title of the current test case.
+  // Use this to name `percySnapshot`s.
+  function testTitle () {
+    return cy.state('runnable').fullTitle()
+  }
+
   // Returns a function that triggers a 'pointermove' event
   // with a new `clientX` relatively shifted by `dX` from
   // that of a target element.
@@ -27,17 +33,20 @@ describe('With a pattern row editor', function () {
     cy.get('.row-expansion-handle')
       .as('handle')
       .trigger('pointerdown', { pointerId })
+    cy.percySnapshot(`${testTitle()} [at pointerdown]`)
     cy.get('.row-expansion-handle-shape')
       .as('handle-shape')
       .should('have.class', 'is-dragged')
     cy.get('@handle')
       .then(triggerPointermoveBy(cellWidth))
+    cy.percySnapshot(`${testTitle()} [at pointermove]`)
     cy.get('@handle')
       .trigger('pointerup', { pointerId })
     cy.get('@handle-shape')
       .should('not.have.class', 'is-dragged')
     cy.get('g.edited-row use')
       .should('have.length', 4)
+    cy.percySnapshot(`${testTitle()} [at pointerup]`)
   })
 
   it('A designer removes a column from a row by dragging the row expansion handle leftward', function () {
@@ -49,30 +58,40 @@ describe('With a pattern row editor', function () {
       .should('have.class', 'is-dragged')
     cy.get('@handle')
       .then(triggerPointermoveBy(-cellWidth))
+    cy.percySnapshot(`${testTitle()} [at pointermove]`)
     cy.get('@handle')
       .trigger('pointerup', { pointerId })
     cy.get('@handle-shape')
       .should('not.have.class', 'is-dragged')
     cy.get('g.edited-row use')
       .should('have.length', 2)
+    cy.percySnapshot(`${testTitle()} [at pointerup]`)
   })
 
   it('A designer releases the row expansion handle before moving it rightward enough to append a column', function () {
     cy.get('.row-expansion-handle')
+      .as('handle')
       .trigger('pointerdown', { pointerId })
       .then(triggerPointermoveBy(Math.floor(cellWidth * 0.5)))
+    cy.percySnapshot(`${testTitle()} [at pointermove]`)
+    cy.get('@handle')
       .trigger('pointerup', { pointerId })
     cy.get('g.edited-row use')
       .should('have.length', 3)
+    cy.percySnapshot(`${testTitle()} [at pointerup]`)
   })
 
   it('A designer releases the row expansion handle before moving it leftward enough to remove a column', function () {
     cy.get('.row-expansion-handle')
+      .as('handle')
       .trigger('pointerdown', { pointerId })
       .then(triggerPointermoveBy(-Math.floor(cellWidth * 0.5)))
+    cy.percySnapshot(`${testTitle()} [at pointermove]`)
+    cy.get('@handle')
       .trigger('pointerup', { pointerId })
     cy.get('g.edited-row use')
       .should('have.length', 3)
+    cy.percySnapshot(`${testTitle()} [at pointerup]`)
   })
 
   it('Dragging of the row expansion handle by a designer is canceled', function () {
@@ -84,5 +103,6 @@ describe('With a pattern row editor', function () {
       .should('not.have.class', 'is-dragged')
     cy.get('g.edited-row use')
       .should('have.length', 3)
+    cy.percySnapshot(`${testTitle()} [at pointercancel]`)
   })
 })
